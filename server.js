4d45d729a890c001e4851b21491d73f180419418
Esteban Saugar <e_saugar_servext_rsi@cajarural.com>
2019-04-29 17:53:04 +0200

    Más cosas

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ mongoose.connect("mongodb+srv://cluster0-f3avm.mongodb.net/test",
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(/^\/sigpes.*$/, bodyParser.json());
 app.use(/^\/sigpes.*$/, sigpesRouter(express));
-app.use(/^(?!sigpes).*$/, express.static('./public'));
+app.use(/^\/(?!sigpes).*$/, express.static('./public'));
 
 app.listen(port);
 
