518f2cc8a4e331c809c7854743d985cc2ad98d20
Esteban Saugar <e_saugar_servext_rsi@cajarural.com>
2019-04-29 10:45:13 +0200

    Más cositas

--- api/routes/sigpesRoutes.js
+++ api/routes/sigpesRoutes.js
@@ -9,7 +9,7 @@ module.exports = (express) => {
     .get(sigpes.list_all_sigpes)
     .post(sigpes.create_a_sigpe);
 
-    router.route('/sigpes/:sigpeId')
+  router.route('/sigpes/:sigpeId')
     .get(sigpes.read_a_sigpe)
     .put(sigpes.update_a_sigpe)
     .delete(sigpes.delete_a_sigpe);
